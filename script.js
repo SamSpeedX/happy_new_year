6e0a8f3e8d586692d2dad9e25d930ce301f51850
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-12-31 20:22:52 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+function countdown() {
+    const newYear = new Date('Jan 1, 2025 00:00:00').getTime();
+    const now = new Date().getTime();
+    const distance = newYear - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById('days').innerText = days;
+    document.getElementById('hours').innerText = hours;
+    document.getElementById('minutes').innerText = minutes;
+    document.getElementById('seconds').innerText = seconds;
+
+    if (distance < 0) {
+        clearInterval(countdownInterval);
+        document.querySelector('.container').innerHTML = '<h1>Happy New Year!</h1>';
+    }
+}
+
+const countdownInterval = setInterval(countdown, 1000);
+countdown();

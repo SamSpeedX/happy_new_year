90d3091249cbafd88253708f3e8dc4cec66a01a7
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2025-01-01 00:27:03 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -22,12 +22,12 @@ function countdown() {
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000); 
+    /*
     document.getElementById('days').innerText = days;
     document.getElementById('hours').innerText = hours;
     document.getElementById('minutes').innerText = minutes;
-    document.getElementById('seconds').innerText = seconds;
+    document.getElementById('seconds').innerText = seconds;*/
     
     if (1 == 1) {
         clearInterval(countdownInterval);
